870435f814d10248657bfad6979ba23ee374e773
fpaul787@gmail.com <fpaul787@gmail.com>
2025-05-28 23:57:16 +0000

    commit

--- gen-ai/LLMs/Evaluating LLMs.py
+++ gen-ai/LLMs/Evaluating LLMs.py
@@ -1,2 +1,12 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Evaluation Metrics
 
+# COMMAND ----------
+
+import torch
+from datasets import load_dataset
+
+full_dataset = load_dataset(
+  "cnn_dailymail", "3.0.0", cache_dir="/dbfs/databricks/datasets"
+)
